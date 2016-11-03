07fc5ded3e098a0d2055d5a379a38ee6d07c7712
Waiting7777 <waiting0819@msn.com>
2016-11-03 18:44:32 +0800

    evolution part update

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,8 +3,13 @@ var Evolution = require('../lib/db_evolution');
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+mongoose.Promise = require('bluebird');
 
 router.get('/:id', function(req, res, next) {
+
+    var temp = [];
 
     if(isNaN(req.params.id)){
         res.send('Can\'t varify string');
@@ -17,13 +22,21 @@ router.get('/:id', function(req, res, next) {
         return;
     }
 
-    Pokemon.find({ PokemonId : id }, { _id : 0, __v : 0 }).exec(function(err, data){
-        console.log(data[0]);
-        res.render('pokemon', {pokemon : data[0]});
-    });
+    Pokemon.find({ PokemonId : id }, { _id : 0, __v : 0 }).exec()
+    .then(function(pokemon){
+        temp['pokemon'] = pokemon[0];
+        return Evolution.find({ Evolution : pokemon[0]['PokemonId']}, { __v : 0, _id : 0 });
+    })
+    .then(function(evolution){
+        if(evolution.length == 0){
+            temp['evolution'] = evolution;
+        }
+        else{
+            temp['evolution'] = evolution[0];
+        }
+        console.log(temp);
 
-    Evolution.find({ Evolution : 5 }).exec(function(err, data){
-        console.log(data);
+        res.render('pokemon', {pokemon : temp['pokemon'], evolution : temp['evolution']});
     });
 
 });
