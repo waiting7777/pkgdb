8e808adb6bee1eda9662e2c2f086e35634b69a99
Waiting7777 <waiting0819@msn.com>
2016-11-10 18:52:21 +0800

    Modify type schema and type part done

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,13 @@
 var Pokemon = require('../lib/db_pokemon');
 var Evolution = require('../lib/db_evolution');
+var Type = require('../lib/db_type');
 var common = require('../lib/common');
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 const bluebird = require('bluebird');
+
+var type_map = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
 
 mongoose.Promise = require('bluebird');
 
@@ -34,9 +37,25 @@ router.get('/:id', function(req, res, next) {
         else{
             temp['evolution'] = evolution[0];
         }
-        console.log(temp);
+        return Type.find({ $or: [ { Type : temp['pokemon']['Type1'] }, {Type : temp['pokemon']['Type2'] } ] }, { _id : 0, __v : 0 });
+    })
+    .then(function(type){
+        var temp_type = [];
+        var temp_type_s = [];
 
-        res.render('pokemon', {pokemon : temp['pokemon'], evolution : temp['evolution']});
+        for(var i=0;i < type[0]['Relate'].length;i++){
+            temp_type[i] = 0;
+        }
+        for(var i in type){
+            for(var j = 0;j < type[i]['Relate'].length;j++){
+                temp_type[j] += parseInt(type[i]['Relate'][j]);
+            }
+        }
+        for(var i=0;i < type[0]['Relate'].length;i+=3){
+            temp_type_s.push(temp_type.slice(i, i+3))
+        }
+        console.log(temp_type_s);
+        res.render('pokemon', {pokemon : temp['pokemon'], evolution : temp['evolution'], types : temp_type_s, type_map : type_map});
     });
 
 });
