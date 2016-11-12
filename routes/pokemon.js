1c43cab6319ad06afdd09324ec85f96f593745d8
Waiting7777 <waiting0819@msn.com>
2016-11-13 01:40:30 +0800

    pokemon page done

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,10 @@
 var Pokemon = require('../lib/db_pokemon');
 var Evolution = require('../lib/db_evolution');
 var Type = require('../lib/db_type');
+var QuickMoveRelate = require('../lib/db_quickmoverelate');
+var QuickMove = require('../lib/db_quickmove');
+var ChargeMoveRelate = require('../lib/db_chargemoverelate');
+var ChargeMove = require('../lib/db_chargemove');
 var common = require('../lib/common');
 var express = require('express');
 var router = express.Router();
@@ -14,6 +18,9 @@ mongoose.Promise = require('bluebird');
 router.get('/:id', function(req, res, next) {
 
     var temp = [];
+    var temp_type = [];
+    var temp_type_s = [];
+    var quickmove_array = [];
 
     if(common.idCheck(req.params.id, 151) == false){
         res.render('error', {
@@ -40,8 +47,6 @@ router.get('/:id', function(req, res, next) {
         return Type.find({ $or: [ { Type : temp['pokemon']['Type1'] }, {Type : temp['pokemon']['Type2'] } ] }, { _id : 0, __v : 0 });
     })
     .then(function(type){
-        var temp_type = [];
-        var temp_type_s = [];
 
         for(var i=0;i < type[0]['Relate'].length;i++){
             temp_type[i] = 0;
@@ -54,10 +59,34 @@ router.get('/:id', function(req, res, next) {
         for(var i=0;i < type[0]['Relate'].length;i+=3){
             temp_type_s.push(temp_type.slice(i, i+3))
         }
-        console.log(temp_type_s);
-        res.render('pokemon', {pokemon : temp['pokemon'], evolution : temp['evolution'], types : temp_type_s, type_map : type_map});
-    });
+        console.log(temp);
+        return QuickMoveRelate.find({ PokemonId : temp['pokemon']['PokemonId']}, { _id : 0, __v : 0 });
 
+    })
+    .then(function(quickmoverelate){
+        var quickrelate_temp = [];
+        for(var i = 0;i < quickmoverelate.length;i++){
+            quickrelate_temp.push(quickmoverelate[i]['QuickMoveId']);
+        }
+        return QuickMove.find({ QuickMoveId : { $in : quickrelate_temp }}, { _id : 0, __v : 0 });
+
+    })
+    .then(function(quickmove){
+        temp['quick'] = quickmove;
+        return ChargeMoveRelate.find({ PokemonId : temp['pokemon']['PokemonId']}, { _id : 0, __v : 0 });
+    })
+    .then(function(chargemoverelate){
+        var chargerelate_temp = [];
+        for(var i = 0;i < chargemoverelate.length;i++){
+            chargerelate_temp.push(chargemoverelate[i]['ChargeMoveId']);
+        }
+        return ChargeMove.find({ ChargeMoveId : { $in : chargerelate_temp }}, { _id : 0, __v : 0 });
+    })
+    .then(function(chargemove){
+        temp['charge'] = chargemove;
+        console.log(temp);
+        res.render('pokemon', {pokemon : temp['pokemon'], evolution : temp['evolution'], types : temp_type_s, type_map : type_map, quick : temp['quick'], charge : temp['charge']});
+    });
 });
 
 module.exports = router;
