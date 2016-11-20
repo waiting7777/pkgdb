4248045297c0f98ef3f93aa75e3f865ca9a8bc0e
Waiting7777 <waiting0819@msn.com>
2016-11-20 23:18:30 +0800

    Quick and Charge page done

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,6 @@ router.get('/', function(req, res, next) {
     Pokemon.find({}, { PokemonId : 1, NameEn : 1, NameTw : 1, Type1 : 1, Type2 : 1, MaxCp : 1, _id : 0}).sort({ PokemonId : 1 }).exec(function(err, pokemons){
         if(err) throw err;
 
-        for(var i in pokemons){
-            pokemons[i]['type'] = pokemons[i]['Type1'] + ' ' + pokemons[i]['Type2'];
-        }
-
         res.render('index', {pokemons : pokemons});
     });
 
--- routes/skill.js
+++ routes/skill.js
@@ -1,5 +1,9 @@
 var Quickmove = require('../lib/db_quickmove');
+var QuickMoveRelate = require('../lib/db_quickmoverelate');
 var Chargemove = require('../lib/db_chargemove');
+var ChargeMoveRelate = require('../lib/db_chargemoverelate');
+var Pokemon = require('../lib/db_pokemon');
+var common = require('../lib/common');
 var express = require('express');
 var router = express.Router();
 
@@ -25,11 +29,101 @@ router.get('/quick', function(req, res, next){
     });
 });
 
+router.get('/quick/:id', function(req, res, next){
+
+    var temp = [];
+
+    if(common.idCheck(req.params.id, 41) == false){
+        res.render('error', {
+            message: 'ERROR',
+            error: 'ERROR'
+        });
+        return;
+    }
+
+    var id = parseInt(req.params.id);
+
+    Quickmove.find({ QuickMoveId : {$in : [id-1, id, id+1]} }, { _id : 0, __v : 0 }).exec()
+    .then(function(quickmove){
+        if(id == 1){
+            temp['quickmove'] = quickmove[0];
+            temp['next'] = quickmove[1]['NameTw'];
+        }
+        else if(id == 41){
+            temp['quickmove'] = quickmove[1];
+            temp['prev'] = quickmove[0]['NameTw'];
+        }
+        else{
+            temp['quickmove'] = quickmove[1];
+            temp['prev'] = quickmove[0]['NameTw'];
+            temp['next'] = quickmove[2]['NameTw'];
+        }
+        return QuickMoveRelate.find({ QuickMoveId : id }, { _id : 0, __v : 0 })
+    })
+    .then(function(quickmoverelate){
+        var pokemon_temp = [];
+        for(var i = 0;i < quickmoverelate.length;i++){
+            pokemon_temp.push(quickmoverelate[i]['PokemonId']);
+        }
+        return Pokemon.find({ PokemonId : { $in : pokemon_temp }}, { _id : 0, __v : 0 }).sort( { MaxCp : -1 });
+
+    })
+    .then(function(pokemons){
+        console.log(pokemons)
+        res.render('quick_skill', {quick : temp['quickmove'], prev : temp['prev'], next : temp['next'], pokemon : pokemons });
+    })
+});
+
 router.get('/charge', function(req, res, next){
     Chargemove.find({}, { _id : 0, __v : 0}).exec()
     .then(function(chargemoves){
         res.render('charge_index', {charges : chargemoves});
     });
+});
+
+router.get('/charge/:id', function(req, res, next){
+
+    var temp = [];
+
+    if(common.idCheck(req.params.id, 41) == false){
+        res.render('error', {
+            message: 'ERROR',
+            error: 'ERROR'
+        });
+        return;
+    }
+
+    var id = parseInt(req.params.id);
+
+    Chargemove.find({ ChargeMoveId : {$in : [id-1, id, id+1]} }, { _id : 0, __v : 0 }).exec()
+    .then(function(chargemove){
+        if(id == 1){
+            temp['chargemove'] = chargemove[0];
+            temp['next'] = chargemove[1]['NameTw'];
+        }
+        else if(id == 41){
+            temp['chargemove'] = chargemove[1];
+            temp['prev'] = chargemove[0]['NameTw'];
+        }
+        else{
+            temp['chargemove'] = chargemove[1];
+            temp['prev'] = chargemove[0]['NameTw'];
+            temp['next'] = chargemove[2]['NameTw'];
+        }
+        return ChargeMoveRelate.find({ ChargeMoveId : id }, { _id : 0, __v : 0 })
+    })
+    .then(function(chargemoverelate){
+        var pokemon_temp = [];
+        for(var i = 0;i < chargemoverelate.length;i++){
+            pokemon_temp.push(chargemoverelate[i]['PokemonId']);
+        }
+        return Pokemon.find({ PokemonId : { $in : pokemon_temp }}, { _id : 0, __v : 0 }).sort( { MaxCp : -1 });
+
+    })
+    .then(function(pokemons){
+        console.log(pokemons)
+        res.render('charge_skill', {charge : temp['chargemove'], prev : temp['prev'], next : temp['next'], pokemon : pokemons });
+    })
 });
 
 module.exports = router;
